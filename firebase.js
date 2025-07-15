a408dc609d6e6ae8f8a696286a751cad33893f49
Davinto4 <eroticoden@gmail.com>
2025-07-15 08:33:35 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,22 @@
-// firebase.js for DavintoReads
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, push, get, child, update, onValue } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
-// Import the functions you need from the SDKs you need import { initializeApp } from "firebase/app"; import { getAuth } from "firebase/auth"; import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyAm9EEBQu4uKoSwIxvpMY8-tbGdSE4KPLk",
+  authDomain: "davintoreads-f34b6.firebaseapp.com",
+  projectId: "davintoreads-f34b6",
+  storageBucket: "davintoreads-f34b6.appspot.com",
+  messagingSenderId: "96533460754",
+  appId: "1:96533460754:web:ca07eed54c6843cfc865cb",
+  databaseURL: "https://davintoreads-f34b6-default-rtdb.firebaseio.com"
+};
 
-// Your web app's Firebase configuration const firebaseConfig = { apiKey: "AIzaSyAm9EEBQu4uKoSwIxvpMY8-tbGdSE4KPLk", authDomain: "davintoreads-f34b6.firebaseapp.com", projectId: "davintoreads-f34b6", storageBucket: "davintoreads-f34b6.firebasestorage.app", messagingSenderId: "96533460754", appId: "1:96533460754:web:ca07eed54c6843cfc865cb" };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
 
-// Initialize Firebase const app = initializeApp(firebaseConfig); const auth = getAuth(app); const db = getFirestore(app);
-
-export { auth, db };
+// Export for use across site
+export { auth, db, ref, set, push, get, child, update, onValue };
