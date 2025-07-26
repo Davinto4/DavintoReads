4d9aa98f4ccd3c3b299fe1b1a3e5119aca83bc8b
Davinto4 <eroticoden@gmail.com>
2025-07-26 16:10:34 +0100

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,3 +1,8 @@
 import ProfilePage from './pages/ProfilePage';
 
 <Route path="/profile" element={<ProfilePage />} />
+import AuthorProfile from "./pages/AuthorProfile";
+import EditProfile from "./pages/EditProfile";
+
+<Route path="/author/:authorId" element={<AuthorProfile />} />
+<Route path="/edit-profile" element={<EditProfile />} />
